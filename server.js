94a1fa76144342a82e26876f5285b038b1f6d3fb
linnovel <luislinnovel@gmail.com>
2025-05-10 21:12:07 -0400

    test render

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
-import path from 'path'
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { connectDb } from './config/db.js'
 import productRoutes from './routes/product.routes.js'
 
@@ -9,7 +10,8 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
-const __dirname = path.resolve()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors())
 app.use(express.json())
